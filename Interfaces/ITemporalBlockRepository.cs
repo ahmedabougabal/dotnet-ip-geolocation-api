c0f5a971969e90ce63c3332dc2561c5c9f41dff0
ahmedabougabal <ahmedabougabal@live.com>
2025-02-26 22:47:12 +0200

    feat: add temporal block repository interface for time-limited blocks

---
+++ Interfaces/ITemporalBlockRepository.cs
@@ -0,0 +1,12 @@
+using CountryBlockingAPI.Models;
+
+namespace CountryBlockingAPI.Interfaces;
+
+public interface ITemporalBlockRepository
+{
+    Task<bool> AddTemporalBlockAsync(TemporalBlock temporalBlock);
+    Task<bool> RemoveTemporalCountryAsync(string countryCode);
+    Task<bool> IsCountryTemporallyBlockedAsync(string countryCode);
+    Task<IEnumerable<TemporalBlock>> GetExpiredTemporalBlocksAsync();
+    Task<IEnumerable<string>> GetAllTemporallyBlockedCountryCodesAsync();
+}
