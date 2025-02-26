b48e39d97902b5b0a7d3170f2c9ea5ff811fe974
ahmedabougabal <ahmedabougabal@live.com>
2025-02-26 22:09:29 +0200

    feat: adds interface for getting a country's info based on an ip address and verify if this country is blocked or not applies interface segregation and separation of concerns supports requirement 4 and 5

---
+++ Interfaces/IGeolocationService.cs
@@ -0,0 +1,12 @@
+using CountryBlockingAPI.Models; 
+
+namespace CountryBlockingAPI.Interfaces;
+
+public interface IGeolocationService
+{
+    Task<CountryInfo?> GetCountryInfoByIpAsync(string ipAddress); // the use of task --> async operation for the HTTP CALLS to the external ipapi.co
+
+}
+
+
+
