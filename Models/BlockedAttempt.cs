c3cca71815c1a09c77c86f7fad37deca61b2a8f3
ahmedabougabal <ahmedabougabal@live.com>
2025-02-28 00:13:24 +0200

    fix: correct a small typo 's' in the Timestamp to be consistent with other files

--- Models/BlockedAttempt.cs
+++ Models/BlockedAttempt.cs
@@ -4,7 +4,7 @@ public class BlockedAttempt
 {
     public string IpAddress { get; set; } = string.Empty; // ensures the properties are never null as a result of using
     //an in-memory storage as required in the assignment 
-    public DateTime TimeStamp { get; set; } = DateTime.UtcNow; // the same cause why we have used '.Utc' here as to prevent null reference exceptions
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow; // the same cause why we have used '.Utc' here as to prevent null reference exceptions
     public string CountryCode { get; set; } = string.Empty;
     public bool IsBlocked { get; set; }
     public string UserAgent { get; set; } = string.Empty;
